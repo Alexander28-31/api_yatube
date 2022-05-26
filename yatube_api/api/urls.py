9b5ba705dba36ca64998be20ed9c3148b12dcf4e
Alexander28-31 <bednenko1604@gmail.com>
2022-05-26 22:19:45 +0300

    Proj

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,7 +9,8 @@ router = DefaultRouter()
 
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
-router.register(r'posts/(?P<post_pk>\d+)/comments', CommentViewSet, basename='comments')
+router.register(r'posts/(?P<post_pk>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
